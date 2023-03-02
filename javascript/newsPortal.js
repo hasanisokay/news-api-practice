e719b8853ed6cff0ff1d178f82b3aa18b427e9e6
hasanisokay <hasanisokay@gmail.com>
2023-03-02 10:48:33 +0600

    added all news counter alert

--- javascript/newsPortal.js
+++ javascript/newsPortal.js
@@ -1,5 +1,18 @@
 const fetchCategories = ()=>{
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayCategories(data.data.news_category))
+}
+// {category_id: '01', category_name: 'Breaking News'}
+const displayCategories = categories =>{
+    const categoriesContainer = document.getElementById('categories-container');
+    categories.forEach(s => {
+        categoriesContainer.innerHTML +=`
+        <p class = "nav-link" onclick = "displayAlert(${s.category_id},'${s.category_name}')" >${s.category_name}</p>
+        `        
+    });
+}
+const displayAlert = (category_id, category_name) => {
+    document.getElementById('alert-count').innerText = category_id;
+    document.getElementById('alert-category').innerText = category_name; 
 }
